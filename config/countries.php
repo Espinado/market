9c342f7b1dab0021d04e191e921a42b62317bba0
Espinado <roman.vyshedko@inbox.lv>
2021-09-12 16:08:46 +0300

    Jquery for delare company registration form

--- config/countries.php
+++ config/countries.php
@@ -17,55 +17,143 @@ return [
      * @var array
      */
 
-    'country_name' => [
+    'name' => [
         /*
-         * Key is the Laravel locale code
-         * Index 0 of sub-array is the Carbon locale code
-         * Index 1 of sub-array is the PHP locale code for setlocale()
-         * Index 2 of sub-array is true if the language uses RTL (right-to-left)
-         * Index 3 of sub-array is the language name in the original language
+         * Key is the Laravel country
+         * Index 0 of sub-array country regions
+
          */
 
-            'Latvia' => [
-                'status'=>true,
-            'code' => 'LV',
-            'name'=>'Latvia',
+        0 => [
+            'country_name' => 'Latvia',
+            'status' => true,
+            'country_code' => 'LV',
             'currency' => 'EUR',
+            'post_index' => 'LV',
             'regions' => [
-                'Riga' => [
+                0 => [
+                    'city_name' => 'Riga',
+                    'streets' => [
+                        'Stabu',
+                        'Brivibas',
+                        'Valdemara'
+                    ],
                     'post_codes' => [
                         '1009',
                         '1010',
                         '1015'
                     ],
                 ],
-                'Jurmala' => [
+
+                1 => [
+                    'city_name' => 'Jurmala',
+                    'streets' => [
+                        'Skolas',
+                        'Raina',
+                        'Talsu'
+                    ],
                     'post_codes' => [
                         '2009',
                         '2010',
                         '2015'
                     ],
                 ],
-                'Kuldiga',
-                'Tukums',
-                'Ventspils'
+
+                2 => [
+                    'city_name' => 'Kuldiga',
+                    'streets' => [
+                        'Kuldigas',
+                        'Rigas',
+                        'Rojas'
+                    ],
+                    'post_codes' => [
+                        '3009',
+                        '3010',
+                        '3015'
+                    ],
+                ],
+
+                3 => [
+                    'city_name' => 'Tukums',
+                    'streets' => [
+                        'Tukuma 1',
+                        'Tukuma 2',
+                        'Kemeru pr.'
+                    ],
+                    'post_codes' => [
+                        '4009',
+                        '4010',
+                        '4015'
+                    ],
+                ],
+
+                4 => [
+                    'city_name' => 'Ventspils',
+                    'streets' => [
+                        'Ventspils 1',
+                        'Ventspils 2',
+                        'Ventspils 3'
+                    ],
+                    'post_codes' => [
+                        '5009',
+                        '5010',
+                        '5015'
+                    ],
+                ],
             ],
         ],
-        'Estonia' => [
-            'status'=>true,
-            'code' => 'EE',
-            'name'=>'Estonia',
+        1 => [
+            'country_name' => 'Estonia',
+            'status' => true,
+            'country_code' => 'EE',
             'currency' => 'EUR',
+            'post_index' => 'LV',
             'regions' => [
-                'Tallinn',
-                'Virtsu',
-                'Hapsalu',
-                'Parnu',
-                'Vormsi'
-            ]
+
+
+                0 => [
+                    'city_name' => 'Tallinn',
+                    'streets' => [
+                        'Tallinn 1',
+                        'Tallinn 2',
+                        'Tallinn 3'
+                    ],
+                    'post_codes' => [
+                        '2009EE',
+                        '2010EE',
+                        '2015EE'
+                    ],
+                ],
+
+                1 => [
+                    'city_name' => 'Virtsu',
+                    'streets' => [
+                        'Virtsu 1',
+                        'Virtsu 2',
+                        'Virtsu 3'
+                    ],
+                    'post_codes' => [
+                        '3009EE',
+                        '3010EE',
+                        '3015EE'
+                    ],
+                ],
+
+                2 => [
+                    'city_name' => 'Hapsalu',
+                    'streets' => [
+                        'Virtsu 1',
+                        'Virtsu 2',
+                        'Virtsu 3'
+                    ],
+                    'post_codes' => [
+                        '2009',
+                        '2010',
+                        '2015'
+                    ],
+                ],
+            ],
         ],
     ],
-
-
 
 ];
--- resources/views/Management/Owner/Dealers_info/add_dealer_company_form.blade.php
+++ resources/views/Management/Owner/Dealers_info/add_dealer_company_form.blade.php
@@ -5,11 +5,96 @@
 @include('management.owner.layouts.includes.headpanel')
 @include('management.owner.layouts.includes.leftpanel')
 @include('management.owner.layouts.includes.rightpanel')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+      $('select[name="company_country"]').on('change',function(){
+         var country_code=$('select[name="company_country"]').val();
+        if (country_code) {
+            array_code=($(this).find(':selected').data('array'));// find index number of country in config
+            $('#reg_country_code').val(array_code) //inset country name
+            $('#post_country_code').val(array_code)
+       var get_statuses = <?php echo json_encode(Config::get('company_legal_status.legal_status')); ?>;
+
+    //    Company legal statuses from config
+
+    country=<?php echo json_encode(Config::get('countries.name')); ?>;//findcountry name by index in config
+     country=country[country_code]['country_name'];
+
+      $('#company_status').empty();
+      $('#company_street').empty();
+      $('#company_reg_country').empty();
+      $('#company_city').empty() //if country changed, these fields should be reset
+      $('#company_post_code').empty();
+
+      $('#company_reg_country').val(country)
+      $('#company_status').append('<option label="Choose status"></option>')
+      $('#company_city').append('<option label="Choose city"></option>')
+      $('#company_street').append('<option label="No city selected"></option>')
+      $('#company_post_code').append('<option label="No city selected"></option>')
+
+
+      $.each(get_statuses[country_code]['status'], function(i,s){
+          $('#company_status').append($('<option>',  {
+        value: i,
+        text :s
+    }));
+});
+    // list of company legal statuses for country from config
+        //-------------------------------------------------------
+    get_cities=<?php echo json_encode(Config::get('countries.name')); ?>;
+    $.each(get_cities[country_code]['regions'], function(key,value){
+    $('#company_city').append($('<option>',  {
+        value: key,
+        text :value['city_name']
+    }));
+
+        // { key : key }).text(value['city_name']));
+
+  });//list of cities in country from config
+//----------------------------------------------------------
+$('#company_city').on('change',function(){
+$('#company_street').empty();
+$('#company_post_code').empty();
+var city_array_code=$('#company_city').val();
+
+        $('#company_street').append('<option label="Choose street"></option>')
+        $('#company_post_code').append('<option label="Choose index"></option>')
+        get_streets= <?php echo json_encode(Config::get('countries.name')); ?>;
+
+         $.each(get_streets[country_code]['regions'][city_array_code]['streets'], function (str, st) {
+        $('#company_street').append($('<option>', {
+        value: str,
+        text : st
+    }));
+          });
+          $.each(get_streets[country_code]['regions'][city_array_code]['post_codes'], function (c, code) {
+        $('#company_post_code').append($('<option>', {
+        value: c,
+        text : code
+    }));
+          });
+})
+      }
+      else {
+        $('#company_status').empty();
+      $('#company_status').append('<option label="No country selected"></option>')
+      }
+    });
+
+    })
+
+</script>
+
+
 
 <!-- ########## START: MAIN PANEL ########## -->
 <div class="sl-mainpanel">
 
-
+    <form method="post" action="{{ route('store.dealer.company') }}" enctype="multipart/form-data">
+                    @csrf
     <div class="sl-pagebody">
       <div class="sl-page-title">
         <h5>Company information</h5>
@@ -22,71 +107,98 @@
 
         <div class="form-layout">
           <div class="row mg-b-25">
-            <div class="col-lg-4">
+            <div class="col-lg-3">
+                <div class="form-group mg-b-10-force">
+  {{-- Getting country list from config file --}}
+
+                  <label class="form-control-label">Country: <span class="tx-danger">*</span></label>
+                  <select class="form-control select2" data-placeholder="Choose country" name="company_country" required>
+                    <option label="Choose country"></option>
+                    @foreach (Config::get('countries.name') as $key=> $value)
+
+                    <option value="{{$key}}" data-array="{{$value['country_code']}}">{{$value['country_name']}}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div><!-- col-3-->
+
+              <div class="col-lg-3">
+           <label class="form-control-label">Company legal status: <span class="tx-danger">*</span></label>
+           <select class="form-control select2" data-placeholder="Choose country" name="company_status" required id="company_status">
+           <option label="No country selected"></option>
+           </select>
+           </div><!-- col-3 -->
+            <div class="col-lg-3">
               <div class="form-group">
-                <form method="post" action="{{ route('store.dealer.company') }}" enctype="multipart/form-data">
-                    @csrf
+
                 <label class="form-control-label">Company name: <span class="tx-danger">*</span></label>
                 <input class="form-control" type="text" name="company_name" placeholder="Enter Company name" required>
               </div>
-            </div><!-- col-4 -->
-            <div class="col-lg-4">
+            </div><!-- col-3 -->
+            <div class="col-lg-3">
               <div class="form-group">
                 <label class="form-control-label">Registration number: <span class="tx-danger">*</span></label>
-                <input class="form-control" type="text" name="company_reg_number" placeholder="Enter registration number" required>
+                <div class="d-md-flex pd-y-20 pd-md-y-0">
+                <input class="col-lg-2" type="text" id="reg_country_code"  disabled>
+               <input class="form-control" type="text" id="company_reg_number" name="company_reg_number" placeholder="Enter registration number" required>
+                </div>
               </div>
-            </div><!-- col-4 -->
-            <div class="col-lg-4">
-              <div class="form-group">
-                <label class="form-control-label">Email address: <span class="tx-danger">*</span></label>
-                <input class="form-control" type="email" name="company_email" placeholder="Enter email address" required>
-              </div>
-            </div><!-- col-4 -->
-            <div class="col-lg-8">
-              <div class="form-group mg-b-10-force">
-                <label class="form-control-label">Mail Address: <span class="tx-danger">*</span></label>
-                <input class="form-control" type="text" name="company_address" placeholder="Enter address" required>
-              </div>
-            </div><!-- col-8 -->
-            <div class="col-lg-4">
-              <div class="form-group mg-b-10-force">
+            </div><!-- col-3 -->
 
 
-                <label class="form-control-label">Country: <span class="tx-danger">*</span></label>
-                <select class="form-control select2" data-placeholder="Choose country" name="company_country" required>
-                  <option label="Choose country"></option>
-                  @foreach (Config::get('countries.country_name') as $tmp)
-                  <option value="{{$tmp['code']}}">{{$tmp['name']}}</option>
-                  @endforeach
-                </select>
-              </div>
-            </div><!-- col-4 -->
+
+
+
+
+
           </div><!-- row -->
 
 
         </div><!-- form-layout -->
+
       </div><!-- card -->
+
       <div class="card pd-20 pd-sm-40 mg-t-50">
-        <h6 class="card-body-title">Company status</h6>
+        <h6 class="card-body-title">Company information</h6>
+        <p class="mg-b-20 mg-sm-b-30">A form with a label on top of each form control.</p>
 
         <div class="row">
-          <div class="col-lg-3">
-            <label class="ckbox">
-                <input type="hidden" name="tax_payer" value="0">
-              <input type="checkbox" name="tax_payer" value="1"><span>Tax payer</span>
-            </label>
-          </div><!-- col-3 -->
-          <div class="col-lg-3">
-            <label class="ckbox">
-                <input type="hidden" checked name="is_active" value="0">
-              <input type="checkbox" checked name="is_active" value="1"><span>is active</span>
-            </label>
-          </div><!-- col-3 -->
+            <div class="col-lg-3">
+                <div class="form-group mg-b-10-force">
+
+                  <label class="form-control-label">Country: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="company_reg_country" id="company_reg_country" disabled value="No country selected">
+                </div>
+              </div><!-- col-3-->
+
+              <div class="col-lg-3">
+           <label class="form-control-label">Company City: <span class="tx-danger">*</span></label>
+
+
+           <select class="form-control select2" data-placeholder="Choose city" name="company_city" id="company_city" required>
+                    <option label="No country selected"></option>
+           </select>
+
+           </div><!-- col-3 -->
+           <div class="col-lg-3">
+           <label class="form-control-label">Company street: <span class="tx-danger">*</span></label>
+           <select class="form-control select2" data-placeholder="Choose city" name="company_street" id="company_street" required>
+                    <option label="No city selected"></option>
+           </select>
+            </div><!-- col-3 -->
+            <div class="col-lg-3">
+           <label class="form-control-label">Company post code: <span class="tx-danger">*</span></label>
+           <div class="d-md-flex pd-y-20 pd-md-y-0">
+                <input class="col-lg-2" type="text" id="post_country_code"  disabled>
+           <select class="form-control select2" data-placeholder="Choose index" name="company_post_code" id="company_post_code" required>
+                    <option label="No city selected"></option>
+           </select>
+           </div>
+            </div><!-- col-3 -->
+
         </div><!-- row -->
 
-
       </div><!-- card -->
-
 
       <div class="card pd-20 pd-sm-40 mg-t-20">
         <h6 class="card-body-title">Form Alignment</h6>
@@ -98,14 +210,17 @@
             <button class="btn btn-secondary">Cancel</button>
           </div>
         </div><!-- d-flex -->
+        </form>
 
 
-      </div><!-- card -->
 
     </div><!-- sl-pagebody -->
 
   </div><!-- sl-mainpanel -->
   <!-- ########## END: MAIN PANEL ########## -->
+
+
 @endsection
+
 
 
